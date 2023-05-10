9426b7c51e7e5d9a0053da103b2e2809a3d9f5cb
Mfaza13 <muhmdfaza@gmail.com>
2023-05-10 14:43:45 +0700

    berhasil menampilkan semua data mhs

--- controller.js
+++ controller.js
@@ -6,3 +6,14 @@ var connection = require('./connect');
 exports.index = function(req, res){
     response.ok("Aplikasi REST API Berjalan",res)
 };
+
+//tampil semua data mhs
+exports.tampilsemuamhs = function(req, res){
+    connection.query('SELECT * FROM tes', function(error, rows, fields){
+        if(error){
+            connection.log(error);
+        } else{
+            response.ok(rows, res)
+        }
+    });
+};
