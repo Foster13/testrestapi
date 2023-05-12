c1a685820f6eaf8bdfd448d9e524f2e42b4061c9
Mfaza13 <muhmdfaza@gmail.com>
2023-05-12 23:05:50 +0700

    register user

---
+++ middleware/auth.js
@@ -0,0 +1,45 @@
+var connection = require('../connect');
+var mysql = require('mysql');
+var md5 = require('md5');
+var response = require('../rest'); //seharusnya nama file res.js
+var jwt = require('jsonwebtoken');
+var config = require('./config/secret.js');
+var ip = require('ip');
+
+//controller utk register
+exports.registrasi = function(req, res){
+    var post = {
+        username: req.body.username,
+        email: req.body.email,
+        password: md5(req.body.password),
+        role: req.body.role,
+        tgl_daftar: new Date()
+    }
+
+    var query = "SELECT email FROM ?? WHERE ??";
+    var table = ["user", "email", post.email];
+
+    query = mysql.format(query, table);
+
+//connection ngecek di var table line 20. jika blm terdaftar melakukan insert into (meniympan data ke table user) dengan data dari post    
+    connection.query(query, function(error, rows){
+        if(error){
+            console.log(error);
+        } else{
+            if(rows.length == 0){ //jika kosong maka dilakukan inser into
+                var query = "INSERT INTO ?? SET ?";
+                var table = ["user"];
+                query = mysql.format(query, table);
+                connection.query(query, post, function(error, rows){
+                    if(error){
+                        console.log(error);
+                    } else{
+                        response.ok("Berhasil menambahkan data user terbaru", res);
+                    }
+                });
+            } else { //jika sudah ada
+                response.ok("Email sudah ada!");
+            }
+        }
+    })
+}
